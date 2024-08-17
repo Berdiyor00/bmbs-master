1247d7034b811443dc8b750308e8a6c6c008632f
Berdiyor00 <berdiyorhasanov1@gmail.com>
2024-08-17 10:22:06 +0500

    loxayla

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PDFWithQRCode from '../src/pdf';
+
+function App() {
+  return (
+    <div className="App">
+      <PDFWithQRCode />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pdf.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import QRCode from 'qrcode.react';
+
+const PDFWithQRCode = () => {
+  return (
+    <div style={{ position: 'relative', width: '100%', height: '100vh' }}>
+      {/* Embed the PDF file */}
+      <embed 
+        src="/bachelor-answersss.pdf" 
+        type="application/pdf" 
+        style={{ width: '100%', height: '100%' }} 
+      />
+      {/* QR code overlay */}
+      <div 
+        style={{ 
+          position: 'absolute', 
+          top: '54px', // Adjust the top value to position correctly
+          left: '440px', // Adjust the left value to position correctly
+          background: 'white', 
+          padding: '10px', 
+          borderRadius: '8px', 
+          boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', 
+          display:'none'
+        }}
+      >
+        <QRCode value="https://bmbs.vercel.app/" size={128} />
+      </div>
+    </div>
+  );
+};
+
+export default PDFWithQRCode;
